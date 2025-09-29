316b428fdec24d4ca9f22ce12d06c954cd07c11f
ziaul-hoque4820 <ziaulhoque4820@gmail.com>
2025-09-29 15:56:50 +0600

    Responsive only laptop size

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,12 @@
 const header = document.querySelector("header");
+const menu = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
 
 window.addEventListener('scroll', () => {
     header.classList.toggle('sticky', window.scrollY > 60)
 });
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
